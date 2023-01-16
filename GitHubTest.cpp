c0e3f3df333597c185233f8bb186750438d25be8
WallyGotGhub <122716287+WallyGotGhub@users.noreply.github.com>
2023-01-17 12:08:59 +1300

    Add files via upload

---
+++ GitHubTest.cpp
@@ -0,0 +1,18 @@
+// GitHubTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("Hello world\n\n");
+// comment to trigger src change and so test merge/commit/upload to github
+	// could not get it to github yesterday, and this now from sven edit and will try again now
+	// SYNC shows up on TeamExplorer Home panel and on use came up with Windows login auth - tried both wallys MS and local and but not worked
+	// did not try local admin login so will try that now
+
+
+	return 0;
+}
+
